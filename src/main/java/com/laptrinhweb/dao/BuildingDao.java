e600af2fa7389808df69b7364f4d6c44624cab5f
ajs95179-beep <ajs95179@gmail.com>
2026-02-22 17:58:19 +0900

    Update BuildingDao.java

--- src/main/java/com/laptrinhweb/dao/BuildingDao.java
+++ src/main/java/com/laptrinhweb/dao/BuildingDao.java
@@ -6,8 +6,7 @@ import com.laptrinhweb.buildingInput.BuildingSearchInput;
 import com.laptrinhweb.buildinganhyeuem.BuildingDaoAnhyeuem;
 
 public interface BuildingDao {
-List<BuildingDaoAnhyeuem> findBuilding(String name,String street,String district,String type);
-List<BuildingDaoAnhyeuem>findBuilding(BuildingSearchInput buildingSearch);
+List<BuildingDaoAnhyeuem> findBuilding(String name,String street,String district,String type	)
 //Long insert(BuildingDaoAnhyeuem buildingDaoAnhyeuem);
 Long insert(BuildingDaoAnhyeuem buildingDaoAnhyeuem,String rentAreas);
 }
